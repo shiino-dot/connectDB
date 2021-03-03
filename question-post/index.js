831e6bb97a0a44d6ec868359f1d1f087af573f74
shiino-dot <shiino.daiki@i-dataone.co.jp>
2021-03-04 06:14:19 +0900

    fixed GetandPost

---
+++ question-post/index.js
@@ -0,0 +1,24 @@
+
+module.exports = function (context, req) {
+  context.log('create question')
+
+  if(req.body) {
+    const item = {
+      "id": req.body.id,
+      "title": req.body.title
+    }
+
+    context.bindings.outputDocument = JSON.stringify(item)
+
+    context.res = {
+      status: 200,
+      body: "success,questions"
+    };
+  } else { 
+    context.res = {
+      status: 400,
+      body: "miss"
+    };
+  }
+  context.done();
+};
